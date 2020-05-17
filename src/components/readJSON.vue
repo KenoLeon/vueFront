a2b022117155b2f1361fe150e584e8e080c729e3
KenoLeon <me@k3no.com>
2020-05-17 14:48:47 -0500

    Add Bootstrap

--- src/components/readJSON.vue
+++ src/components/readJSON.vue
@@ -4,15 +4,28 @@ The template then loads the JSON in a couple of diffferent ways-->
 
 
 <template>
-  <span>
+
+  <div class="container-fluid">
+    <div class="col-sm"></div>
+    <div class="col-sm shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+     Vue +  <strong>bootstrap</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+
+
     <p>JSON DATA: {{ jsonData }}</p>
     <p>JSON DATA: {{ jsonData.people[0]}}</p>
-    <ul>
-      <li v-for="person in jsonData.people" :key="person.name">
+    <ul class="list-group">
+      <li v-for="person in jsonData.people" :key="person.name" class="list-group-item">
      <b>Name:</b> {{ person.name }}, <b>Website:</b> {{ person.website }}, <b>From:</b> {{ person.from }}
       </li>
     </ul>
-  </span>
+  </div>
+  <div class="col-sm"></div>
+  </div>
 </template>
 
 <script>
